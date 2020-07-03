2b38bfe78befc0b8de304ccd4e8c4f066d702651
SimLif <517992857@qq.com>
2020-07-03 20:59:06 +0800

    100bit-Addition

---
+++ Project/2-1-100bit-Addition.asm
@@ -0,0 +1,36 @@
+assume cs:code, ds:data
+
+data segment
+	a dw 0,0001h, 0ffffh, 0000h, 0000h, 0000h, 0000h, 0000h
+	b dw 0,0002h, 0001h, 0000h, 0001h, 0000h, 0000h, 0000h
+data ends
+
+code segment
+	start:
+		mov ax, data
+		mov ds, ax
+		
+		mov ax, b[14]
+		add a[14], ax
+		pushf
+		
+		mov bx, 12
+		stadd:
+			cmp bx, 0
+			jz endadd
+			mov ax, b[bx]
+			popf
+			adc a[bx], ax
+			pushf
+			dec bx
+			dec bx
+			jmp stadd
+			endadd:
+				nop
+		
+		
+		mov ax, 4c00h
+		int 21h
+
+code ends
+end start
